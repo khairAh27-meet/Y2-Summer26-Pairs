3cfa9790697786e9037d3d9861f083895370fcb5
yasminsh27-meet <ysminshaymi19@gmail.com>
2026-07-20 14:21:09 +0300

    weather3

--- app2.py
+++ app2.py
@@ -74,7 +74,7 @@ def run_chat():
     print("You: (type exit to quit)")
 
     system_message = """
-your name is jack, Your job is to provide real travel answers and do not make up facts.
+your name is ward, Your job is to provide real travel answers and do not make up facts.
 
 Rules:
 - Be creative
